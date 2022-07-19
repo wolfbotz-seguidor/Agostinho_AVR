93f2a5e2f3acbc9f1c8f87fb3886967155e5c575
Johann Steffanno Jerônimo da Costa Caniato Amorim <78663503+Johanns123@users.noreply.github.com>
2022-07-19 15:55:39 -0300

    Início do código

---
+++ src/main.c
@@ -0,0 +1,29 @@
+//#define FCPU  16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void setup(void);
+void loop(void);
+
+int main()
+{
+  setup();
+  while(1) loop();
+  return 0;
+}
+
+
+void setup(void)
+{
+
+
+
+}
+
+void loop(void)
+{
+
+
+}
