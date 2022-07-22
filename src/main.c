9c2e6821cbc20ab51def387549bc249749fa9155
Gabriel Camargo <62205909+grocre@users.noreply.github.com>
2022-07-22 16:06:21 -0300

    Comentários para o Renatinho

--- src/main.c
+++ src/main.c
@@ -5,14 +5,23 @@
 #include <avr/interrupt.h>
 #include "macros.h"
 
-void setup_timer0_interrupt(void)
-{ 
-  TCNT0  = 6;    //125us
-
-  /*tempo =65536 * Prescaler/Fosc = 65536 * 1024/16000000 = 4, 19s
+// atmega328p
+/*tempo =65536 * Prescaler/Fosc = 65536 * 1024/16000000 = 4, 19s
     tempo = X_bit_timer * Prescaler/Fosc
     Valor inicial de contagem = 256 - tempo_desejado*Fosc/Prescaler = 256 - 0,001*16000000/1024 = 255
     Valor inicial de contagem = X_bit_timer - tempo_desejado*Fosc/Prescaler */
+
+ISR(TIMER0_OVF_vect)
+{ 
+  TCNT0  = 6;    //125us
+
+  //funcao contador
+    /*
+  
+  mapeamento de pista
+  leitura de sensores 
+
+  */
 }
 
 void setup(void);
@@ -50,11 +59,6 @@ saídas: =PonteH=
   TCCR0B = 0x02; // TC0 com prescaler de 8
   TCNT0  = 6;    // 125us
   TIMSK0 = 0x01; // habilita a interrupcao do TC0
-  
-  /*tempo =65536 * Prescaler/Fosc = 65536 * 1024/16000000 = 4, 19s
-    tempo = X_bit_timer * Prescaler/Fosc
-    Valor inicial de contagem = 256 - tempo_desejado*Fosc/Prescaler = 256 - 0,001*16000000/1024 = 255
-    Valor inicial de contagem = X_bit_timer - tempo_desejado*Fosc/Prescaler */
 
   PWM_init();
   setup_pwm_setFreq(12);
@@ -63,10 +67,6 @@ saídas: =PonteH=
 
 void loop(void)
 {
-  /*
-  
-  mapeamento de pista
-  leitura de sensores 
-
-  */
+  //vazio
+  //utilizar somente para funcoes de baixa prioridade!!!!!!!
 }
